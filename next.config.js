aa9c03ec30aae5aabac54b5463f9269f910daf5b
SkyYap <32660903+SkyYap@users.noreply.github.com>
2025-04-16 19:50:04 +0800

    fix: allow unauthenticated access to mail2.png and mail2-light.png in middleware, update next config and env types

--- next.config.js
+++ next.config.js
@@ -15,6 +15,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'files.stripe.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
     ],
   },
   i18n,
