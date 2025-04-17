7a0a9aec47ee222b375e352907879aae0deedb33
SkyYap <32660903+SkyYap@users.noreply.github.com>
2025-04-17 18:15:32 +0800

    refactor: remove previous landing page code
    remove previous landing page code.

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -27,6 +27,7 @@ import AgreeMessage from '@/components/auth/AgreeMessage';
 import GoogleReCAPTCHA from '@/components/shared/GoogleReCAPTCHA';
 import ReCAPTCHA from 'react-google-recaptcha';
 import { maxLengthPolicies } from '@/lib/common';
+import LoginPage from '@/lib/components/login';
 
 interface Message {
   text: string | null;
@@ -36,193 +37,12 @@ interface Message {
 const Login: NextPageWithLayout<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ csrfToken, authProviders, recaptchaSiteKey }) => {
-  const router = useRouter();
-  const { status } = useSession();
-  const { t } = useTranslation('common');
-  const [recaptchaToken, setRecaptchaToken] = useState<string>('');
-  const [message, setMessage] = useState<Message>({ text: null, status: null });
-  const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
-  const recaptchaRef = useRef<ReCAPTCHA>(null);
-
-  const { error, success, token } = router.query as {
-    error: string;
-    success: string;
-    token: string;
-  };
-
-  const handlePasswordVisibility = () => {
-    setIsPasswordVisible((prev) => !prev);
-  };
-
-  useEffect(() => {
-    if (error) {
-      setMessage({ text: error, status: 'error' });
-    }
-
-    if (success) {
-      setMessage({ text: success, status: 'success' });
-    }
-  }, [error, success]);
-
-  const redirectUrl = token
-    ? `/invitations/${token}`
-    : env.redirectIfAuthenticated;
-
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-    },
-    validationSchema: Yup.object().shape({
-      email: Yup.string().required().email().max(maxLengthPolicies.email),
-      password: Yup.string().required().max(maxLengthPolicies.password),
-    }),
-    onSubmit: async (values) => {
-      const { email, password } = values;
-
-      setMessage({ text: null, status: null });
-
-      const response = await signIn('credentials', {
-        email,
-        password,
-        csrfToken,
-        redirect: false,
-        callbackUrl: redirectUrl,
-        recaptchaToken,
-      });
-
-      formik.resetForm();
-      recaptchaRef.current?.reset();
-
-      if (response && !response.ok) {
-        setMessage({ text: response.error, status: 'error' });
-        return;
-      }
-    },
-  });
-
-  if (status === 'loading') {
-    return <Loading />;
-  }
-
-  if (status === 'authenticated') {
-    router.push(redirectUrl);
-  }
-
-  const params = token ? `?token=${token}` : '';
-
   return (
     <>
       <Head>
-        <title>{t('login-title')}</title>
+        <title>Sign In</title>
       </Head>
-      {message.text && message.status && (
-        <Alert status={message.status} className="mb-5">
-          {t(message.text)}
-        </Alert>
-      )}
-      <div className="rounded p-6 border">
-        <div className="flex gap-2 flex-wrap">
-          {authProviders.github && <GithubButton />}
-          {authProviders.google && <GoogleButton />}
-        </div>
-
-        {(authProviders.github || authProviders.google) &&
-          authProviders.credentials && <div className="divider">{t('or')}</div>}
-
-        {authProviders.credentials && (
-          <form onSubmit={formik.handleSubmit}>
-            <div className="space-y-3">
-              <InputWithLabel
-                type="email"
-                label="Email"
-                name="email"
-                placeholder={t('email')}
-                value={formik.values.email}
-                error={formik.touched.email ? formik.errors.email : undefined}
-                onChange={formik.handleChange}
-              />
-              <div className="relative flex">
-                <InputWithLabel
-                  type={isPasswordVisible ? 'text' : 'password'}
-                  name="password"
-                  placeholder={t('password')}
-                  value={formik.values.password}
-                  label={
-                    <label className="label">
-                      <span className="label-text">{t('password')}</span>
-                      <span className="label-text-alt">
-                        <Link
-                          href="/auth/forgot-password"
-                          className="text-sm text-primary hover:text-[color-mix(in_oklab,oklch(var(--p)),black_7%)]"
-                        >
-                          {t('forgot-password')}
-                        </Link>
-                      </span>
-                    </label>
-                  }
-                  error={
-                    formik.touched.password ? formik.errors.password : undefined
-                  }
-                  onChange={formik.handleChange}
-                />
-                <TogglePasswordVisibility
-                  isPasswordVisible={isPasswordVisible}
-                  handlePasswordVisibility={handlePasswordVisibility}
-                />
-              </div>
-              <GoogleReCAPTCHA
-                recaptchaRef={recaptchaRef}
-                onChange={setRecaptchaToken}
-                siteKey={recaptchaSiteKey}
-              />
-            </div>
-            <div className="mt-3 space-y-3">
-              <Button
-                type="submit"
-                color="primary"
-                loading={formik.isSubmitting}
-                active={formik.dirty}
-                fullWidth
-                size="md"
-              >
-                {t('sign-in')}
-              </Button>
-              <AgreeMessage text={t('sign-in')} />
-            </div>
-          </form>
-        )}
-
-        {(authProviders.email || authProviders.saml) && (
-          <div className="divider"></div>
-        )}
-
-        <div className="space-y-3">
-          {authProviders.email && (
-            <Link
-              href={`/auth/magic-link${params}`}
-              className="btn btn-outline w-full"
-            >
-              &nbsp;{t('sign-in-with-email')}
-            </Link>
-          )}
-
-          {authProviders.saml && (
-            <Link href="/auth/sso" className="btn btn-outline w-full">
-              &nbsp;{t('continue-with-saml-sso')}
-            </Link>
-          )}
-        </div>
-      </div>
-      <p className="text-center text-sm text-gray-600 mt-3">
-        {t('dont-have-an-account')}
-        <Link
-          href={`/auth/join${params}`}
-          className="font-medium text-primary hover:text-[color-mix(in_oklab,oklch(var(--p)),black_7%)]"
-        >
-          &nbsp;{t('create-a-free-account')}
-        </Link>
-      </p>
+      <LoginPage />
     </>
   );
 };
